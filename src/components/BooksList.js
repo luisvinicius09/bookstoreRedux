5baa14656b99a0669a0004178646d7453d14c5ed
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-15 21:38:23 -0300

    Update logic to map all books and display categories

---
+++ src/components/BooksList.js
@@ -0,0 +1,35 @@
+const BooksList = () => (
+  <div>
+    <table>
+      <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+      <tbody>
+        <tr>
+          <td>Test 1</td>
+          <td>Test 2</td>
+          <td>Test 3</td>
+        </tr>
+        <tr>
+          <td>Test 1</td>
+          <td>Test 2</td>
+          <td>Test 3</td>
+        </tr>
+        <tr>
+          <td>Test 1</td>
+          <td>Test 2</td>
+          <td>Test 3</td>
+        </tr>
+        <tr>
+          <td>Test 1</td>
+          <td>Test 2</td>
+          <td>Test 3</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
+
+export default BooksList;
