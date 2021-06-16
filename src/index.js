a3395e2d8ba367e53c57b4dcd6cff52af13ba321
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-15 21:38:03 -0300

    Update to connect to redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
+
+const reducer = () => ({
+  test: 'wroking',
+});
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
