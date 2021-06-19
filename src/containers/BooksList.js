df06b6ecee55a1e6243110a0611b469cac04da57
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-19 15:01:23 -0300

    Update list to add logic and button to remove books

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,31 +1,61 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
+import { removeBook } from '../actions/index';
 
-const BooksList = ({ books }) => (
-  <div>
-    <table>
-      <thead>
-        <tr>
-          <td>Book ID</td>
-          <td>Title</td>
-          <td>Category</td>
-          <td>Remove Book</td>
-        </tr>
-      </thead>
-      <tbody>
-        { books.map((book) => <Book key={book.id} book={book} />) }
-      </tbody>
-    </table>
-  </div>
-);
+class BooksList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveBook = this.handleRemoveBook.bind(this);
+  }
+
+  handleRemoveBook(book) {
+    const { removeBook } = this.props;
+    const s = this.props;
+    const bookIndex = (s.books).findIndex((x) => x === book);
+    removeBook(bookIndex);
+  }
+
+  render() {
+    const { books } = this.props;
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>Book ID</td>
+              <td>Title</td>
+              <td>Category</td>
+              <td>Remove Book</td>
+            </tr>
+          </thead>
+          <tbody>
+            { books.map((book) => (
+              <Book
+                key={book.id}
+                book={book}
+                handleRemoveBook={this.handleRemoveBook()}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (s) => ({
   books: s.books,
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  removeBook: (book) => dispatch(removeBook(book)),
+});
+
 BooksList.propTypes = {
   books: PropTypes.instanceOf(Object).isRequired,
+  removeBook: PropTypes.instanceOf(Function).isRequired,
 };
 
-export default connect(mapStateToProps)(BooksList);
+export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
