3d9fa90c14bb5819ef7938bea123d15fd6ef4d37
Luis Vinicius <luisvinicius0906@gmail.com>
2021-06-20 20:30:38 -0300

    Add import line of sass file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import './index.scss';
 import App from './components/App';
 import books from './reducers/index';
 import initialState from './initialState';
